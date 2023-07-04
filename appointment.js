d6fe6c5cb1815fa939e0f6c8937cab2b2c18348e
ValentinReinbold <valentinreinbold.pro@gmail.com>
2023-07-04 22:51:32 +0200

    Introduce Appointment class

---
+++ appointment.js
@@ -0,0 +1,25 @@
+function Appointment(str) {
+    
+    this.id = str.id;
+
+    this.startTime = new Date(str.attributes['start-time']);
+    this.endTime = new Date(str.attributes['end-time']);
+
+    this.name = str.attributes.name;
+    this.agentName = str.attributes['agent-display-name'];
+    this.usecaseID = str.attributes['usecase-id'];
+    this.waitingRoomURL = str.attributes['agent-default-url'];
+    this.scheduled = str.attributes.status == 'SCHEDULED';
+
+    this.toString = function() {
+        return "Appointment #" + this.id
+            + (this.name ? " \"" + this.name + "\"" : '')
+            + (this.scheduled ? '' : ' CANCELLED')
+            + "\nfrom: " + this.startTime.toDateString() + " - " + this.startTime.toTimeString()
+            + "\nuntil: " + this.endTime.toDateString() + " - " + this.endTime.toTimeString()
+            + (this.agentName ? "\nagentName: " + this.agentName : '')
+            + (this.usecaseID ? "\nusecaseID: " + this.usecaseID : '')
+            + (this.waitingRoomURL ? "\nwaitingRoomURL: " + this.waitingRoomURL : '')
+            + "\n";
+    }
+}
