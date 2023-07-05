dadb7d6b5f76397c197aee0255548ce0db21687c
ValentinReinbold <valentinreinbold.pro@gmail.com>
2023-07-05 02:30:20 +0200

    Handle date picker + Adapt table

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
+const morningTime = 9;
+const eveningTime = 17;
+const period = 0.5;
+
+var dayPicker = document.getElementById("day_picker");
 var appointmentTable = document.getElementById("appointment_table");
 
+var day = new Date((new Date()).toDateString());
 var appointments = {};
+
+dayPicker.value = day.getFullYear() + "-"
+    + (day.getMonth() + 1).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping: false}) + "-"
+    + day.getDate().toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping: false});
+
+dayPicker.addEventListener("change", (event) => {
+    day = new Date((new Date(event.target.value)).toDateString());
+    update();
+});
 
 update();
 
@@ -13,40 +28,74 @@ function update() {
 
         var data = result.data;
         if (data && data.length) {
-
             appointments = {};
-            appointments = generateList(data);
+            appointments = generateList(data, day);
 
             var appointmentSortedIDs = Object.keys(appointments).sort((a, b) => {
                 return appointments[a].startTime.getTime() - appointments[b].startTime.getTime();
             });
 
-            appointmentSortedIDs.forEach(id => {
-                console.log(appointments[id].toString());
-                addInTable(appointments[id]);
-            });
+            clearTable();
+            fillTable(appointmentSortedIDs);
         }
-
     });
 }
 
-function generateList(data) {
+function generateList(data, day) {
     var list = {};
     data.forEach(str => {
-        list[str.id] = new Appointment(str);
+        var appointment = new Appointment(str);
+        if (new Date(appointment.startTime.toDateString()) <= day && day <= new Date(appointment.endTime.toDateString())) {
+            if (appointment.startTime.getHours() < eveningTime && morningTime < appointment.endTime.getHours()) {
+                list[str.id] = new Appointment(str);
+            }
+        }
     });
     return list;
 }
 
-function addInTable(appointment) {
-    var str = "<tr>"
-            + "<th>" + appointment.startTime.toLocaleDateString() + "</th>"
-            + "<th>" + appointment.startTime.toLocaleTimeString() + "</th>"
+function clearTable() {
+    while (appointmentTable.children[1]) {
+        appointmentTable.removeChild(appointmentTable.lastChild);
+    }
+}
+
+function fillTable(appointmentSortedIDs) {
+    for (var h = morningTime; h <= eveningTime; h += period) {
+        var hour = Math.floor(h);
+        var minute = (h == hour ? 0 : period*60);
+        var timeStr = hour.toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping: false})
+            + ":" + minute.toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping: false});
+
+        var appointment = null
+        for (var id in appointmentSortedIDs) {
+            appointment = appointments[id];
+            startHour = appointment.startTime.getHours() + appointment.startTime.getMinutes() / 60;
+            endHour = appointment.endTime.getHours() + appointment.endTime.getMinutes() / 60;
+            if (startHour < h + period && h < endHour) {
+                break;
+            }
+            appointment = null;
+        }
+        addRow(timeStr, appointment);
+    }
+}
+
+function addRow(timeStr, appointment) {
+    var str = "<tr><th>" + timeStr + "</th>";
+    if (appointment) {
+        str += "<th>" + (appointment.startTime ? appointment.startTime.toLocaleDateString() : "") + "</th>"
+            + "<th>" + (appointment.startTime ? appointment.startTime.toLocaleTimeString() : "") + "</th>"
             + "<th><a" + (appointment.waitingRoomURL ? " href='" + appointment.waitingRoomURL + "'" : "") + ">"
             + (appointment.name ? "\"" + appointment.name + "\"" : "Untitled")
             + (appointment.agentName ? " - " + appointment.agentName : "")
-            + "</a></th>"
+            + "</a></th>";
+    }
+    else {
+        str += "<th></th>"
             + "<th></th>"
-            + "</tr>";
+            + "<th></th>";
+    }
+    str += "</tr>";
     appointmentTable.insertAdjacentHTML('beforeend', str);
 }
