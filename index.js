81e4a2420465cc4c9f30b4f997a693431e1d2adf
ValentinReinbold <valentinreinbold.pro@gmail.com>
2023-07-05 14:51:30 +0200

    Handle button behaviors

--- index.js
+++ index.js
@@ -35,7 +35,6 @@ function update() {
         var data = result.data;
         var appointmentSortedIDs = [];
         
-        console.log("Listing:", data);
         if (data && data.length) {
             appointments = {};
             appointments = generateList(data, day);
@@ -50,6 +49,8 @@ function update() {
 
         clearTable();
         fillTable(schedule);
+
+        handleButtons(schedule);
     });
 }
 
@@ -124,6 +125,91 @@ function addRow(h, id) {
     }
     str += "</tr>";
     appointmentTable.insertAdjacentHTML('beforeend', str);
+}
+
+function handleButtons(schedule) {
+    var newButtons = document.getElementsByClassName('new_button');
+    var addButtons = document.getElementsByClassName('add_button');
+    var cancelButtons = document.getElementsByClassName('cancel_button');
+    var deleteButtons = document.getElementsByClassName('delete_button');
+
+    for (let button of newButtons) {
+        button.addEventListener("click", (event) => {
+            var h = event.target.value;
+
+            disableButtons(cancelButtons, true);
+            disableButtons(newButtons, true);
+
+            button.style.display = 'none';
+            document.getElementById('add_button_' + h).style.display = 'block';
+            document.getElementById('new_title_' + h).style.display = 'block';
+        });
+    }
+
+    for (let button of addButtons) {
+        button.addEventListener("click", (event) => {
+            var h = event.target.value;
+
+            disableButtons(cancelButtons, false);
+            disableButtons(newButtons, false);
+
+            var startDate = new Date(day);
+            var endDate = new Date(day);
+            startDate.setTime(startDate.getTime() + h/100 * 60 * 60 * 1000);
+            endDate.setTime(endDate.getTime() + (h/100 + period) * 60 * 60 * 1000);
+            
+            var newAppointment = new Appointment({
+                "startTime": startDate,
+                "endTime": endDate,
+                "name": document.getElementById('new_title_' + h).value,
+                "agentName": agentName,
+                "usecaseID": usecaseID,
+                "scheduled": true
+            });
+            createAppointment(newAppointment, (success, result) => {
+                if (!success) {
+                    console.log("Creation:", result);
+                    return;
+                }
+                update();
+            });
+        });
+    }
+
+    for (let button of cancelButtons) {
+        button.addEventListener("click", (event) => {
+            var h = event.target.value;
+            var appointment = appointments[schedule[h]];
+            appointment.scheduled = false;
+            updateAppointment(appointment, (success, result) => {
+                if (!success) {
+                    console.log("Update:", result);
+                    return;
+                }
+                update();
+            });
+        });
+    }
+
+    for (let button of deleteButtons) {
+        button.addEventListener("click", (event) => {
+            var h = event.target.value;
+            var appointment = appointments[schedule[h]];
+            deleteAppointment(appointment, (success, result) => {
+                if (!success) {
+                    console.log("Deletion:", result);
+                    return;
+                }
+                update();
+            });
+        });
+    }
+}
+
+function disableButtons(buttons, state) {
+    for (let button of buttons) {
+        button.disabled = state;
+    }
 }
 
 function deleteAllAppointments(data) {
